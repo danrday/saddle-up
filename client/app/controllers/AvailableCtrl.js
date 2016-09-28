a7d78c71d487bb49e7e1664a6538c16da5ba6a37
Dominic Serrano <dmsrojo@gmail.com>
2016-09-28 14:33:26 -0500

    Added like and dislike user button functionality, still planning

--- client/app/controllers/AvailableCtrl.js
+++ client/app/controllers/AvailableCtrl.js
@@ -1,6 +1,32 @@
 'use strict';
 
-app.controller('AvailableCtrl', function($scope, $http) {
+app.controller('AvailableCtrl', function($scope, $http, $location) {
+
+  //Get the entire db of users
+  // let loadUserList = () => {
+  //   $http
+  //   .get('api/available/:username')
+  //   .then((list) => {
+  //     //Filter the users and do not display disliked users for the current user
+  //     list.forEach((each) => {
+  //       $scope.userList = list.filter((user) => {
+  //         return  ===
+  //       });
+  //     })
+  //   });
+  // }
+
+  $scope.likeUser = () => {
+    $http
+    .put('api/available/:username/:likedusername')
+    .then(() => {
+      //Run the page load display function
+    })
+    .catch()
+
+  };
+
+  $scope.dislikeUser = () => console.log("Test text");
 
 $scope.userList = [
 {
