150bd39567c2dda4bc9646fc209c31944497244d
Dominic Serrano <dmsrojo@gmail.com>
2016-09-28 14:02:49 -0500

    Added navbar and now populating availableView from array with from the controller

--- client/app/controllers/AvailableCtrl.js
+++ client/app/controllers/AvailableCtrl.js
@@ -2,4 +2,30 @@
 
 app.controller('AvailableCtrl', function($scope, $http) {
 
-})
+$scope.userList = [
+{
+  username: 'Greg',
+  species: 'Stud',
+  seeking: 'Steed',
+  location: 'Location 1',
+  description: 'My name is greg'
+},
+{
+  username: 'Greg',
+  species: 'Stud',
+  seeking: 'Steed',
+  location: 'Location 1',
+  description: 'My name is greg'
+},
+{
+  username: 'Greg',
+  species: 'Stud',
+  seeking: 'Steed',
+  location: 'Location 1',
+  description: 'My name is greg'
+}
+];
+
+
+
+});//End AvailableCtrl
