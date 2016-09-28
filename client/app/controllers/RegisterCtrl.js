e667b788848a238c0c60ef34e86b25a94534dfb6
Jack Mocherman <mochermanjack@gmail.com>
2016-09-28 13:32:48 -0500

    added location paths to then of http post

--- client/app/controllers/RegisterCtrl.js
+++ client/app/controllers/RegisterCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('RegisterCtrl', function($scope, $http) {
+app.controller('RegisterCtrl', function($scope, $http, $location) {
 
 	$(document).ready(function() {
 		$('select').material_select()
@@ -11,16 +11,22 @@ app.controller('RegisterCtrl', function($scope, $http) {
 		username: $scope.username,
 		password: $scope.password,
 		location: $scope.location,
+		species:  $scope.species,
+		seeking:  $scope.seeking,
 		description: $scope.description,
-		species: $scope.species,
-		seeking: $scope.seeking
 	}
 
 	console.log(user)
 
 	$http
 		.post('api/register', user)
-		.then(() => {})
+		.then((user) => {
+			if(user) {
+				$location.path('/login')
+			} else {
+				$location.path('/register')
+			}
+		})
 		.catch(console.error)
 	}
 
