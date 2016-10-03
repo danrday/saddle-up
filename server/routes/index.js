00f3ab91d4033c31640e2d4f8f755df0c883e455
Dominic Serrano <dmsrojo@gmail.com>
2016-10-02 21:31:56 -0500

    Fixed deprecation error for session and added a NavCtrl for navbar view

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,6 @@
 
 const { Router } = require('express')
 const bcrypt = require('bcrypt')
-
 const router = Router()
 const User = require('../models/user')
 
--- server/server.js
+++ server/server.js
@@ -5,22 +5,19 @@ const bodyParser = require('body-parser')
 const session = require('express-session')
 const RedisStore = require('connect-redis')(session)
 const {red} = require('chalk')
-
 const routes = require('./routes')
 const { connect } = require('./db/database')
-
 const app = express()
 
 const port = process.env.PORT || 3000
 app.set('port', port)
 
-
-/////////////////////////////////////////
 // middlewares
-
 app.use(express.static('client'))
 
 app.use(session({
+  resave: false,
+  saveUninitialized: false,
   store: new RedisStore({
     url: process.env.REDIS_URL || 'redis://localhost:6379'
   }),
@@ -28,13 +25,9 @@ app.use(session({
 }))
 app.use(bodyParser.json())
 
-/////////////////////////////////////////
 // routes
 app.use(routes)
-/////////////////////////////////////////
 
-
-/////////////////////////////////////////
 //HTML5MODE middleware for any 'api' routes and alternate routes
 app.use('/api', (req, res) => {
   res.status(404).send({message: 'Not found'});
@@ -43,10 +36,7 @@ app.use('/api', (req, res) => {
 app.use((req, res) => {
   res.sendFile(process.cwd() + '/client/index.html');
 });
-/////////////////////////////////////////
 
-
-/////////////////////////////////////////
 // Error handling middleware
 app.use((
     err,
@@ -71,10 +61,7 @@ app.use((
     console.error(err.stack)
   }
 )
-/////////////////////////////////////////
 
-
-/////////////////////////////////////////
 // Listen to requests on the provided port and log when available
 connect()
   .then(() => {
@@ -83,4 +70,3 @@ connect()
     )
   })
   .catch(console.error)
-/////////////////////////////////////////
