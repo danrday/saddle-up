b9e3e28ba9655e94b6a7aaa878dcf696898a6985
Dominic Serrano <dmsrojo@gmail.com>
2016-10-02 21:52:06 -0500

    Added logic to determine which tabs to show on nave bar depending on whether or not a user is signed in

--- client/app/controllers/LoginCtrl.js
+++ client/app/controllers/LoginCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('LoginCtrl', function($scope, $http, $location, $routeParams, UserFactory, AuthFactory) {
+app.controller('LoginCtrl', function($scope, $rootScope, $http, $location, $routeParams, UserFactory, AuthFactory) {
 
 	//Message to show for failed login
 	$scope.failedLogin = true;
@@ -10,6 +10,7 @@ app.controller('LoginCtrl', function($scope, $http, $location, $routeParams, Use
 	/////////////////////////////////////////
 	//User login functionality
 	$scope.login = function() {
+
 		const user = {
 			username: $scope.username,
 			password: $scope.password
@@ -21,6 +22,8 @@ app.controller('LoginCtrl', function($scope, $http, $location, $routeParams, Use
 			 	//If user exists sign them in
 			 	if (data.data.user) {
 					$location.path('/');
+					//If login is a success, show full navbar
+					$rootScope.showUserNavLinks();
 				//If user does not exist
 				//Reset form and show error message
 				} else {
